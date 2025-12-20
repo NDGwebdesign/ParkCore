79e5ae6e3a37d1f5da51c76ce66c4f060e58e791
dafrijder <nielsdegast@outlook.com>
2025-12-20 11:19:25 +0100

    loading screen

--- src/main/java/net/ndgwebdesign/parkCore/ParkCore.java
+++ src/main/java/net/ndgwebdesign/parkCore/ParkCore.java
@@ -1,17 +1,51 @@
 package net.ndgwebdesign.parkCore;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ParkCore extends JavaPlugin {
 
+    private static ParkCore instance;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
+        printStartupBanner();
+
+        saveDefaultConfig();
+
+        Bukkit.getLogger().info("[ParkCore] Successfully enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getLogger().info("[ParkCore] Disabling ParkCore...");
+
+        Bukkit.getLogger().info("[ParkCore] Successfully disabled!");
     }
+
+    /* ---------------------------------------------------------------------- */
+    /* Utility                                                                 */
+    /* ---------------------------------------------------------------------- */
+
+    private void printStartupBanner() {
+        getLogger().info(" ");
+        getLogger().info(" ██████╗  █████╗ ██████╗ ██╗  ██╗ ██████╗ ██████╗ ███████╗");
+        getLogger().info(" ██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝██╔═══██╗██╔══██╗██╔════╝");
+        getLogger().info(" ██████╔╝███████║██████╔╝█████╔╝ ██║   ██║██████╔╝█████╗  ");
+        getLogger().info(" ██╔═══╝ ██╔══██║██╔══██╗██╔═██╗ ██║   ██║██╔══██╗██╔══╝  ");
+        getLogger().info(" ██║     ██║  ██║██║  ██║██║  ██╗╚██████╔╝██║  ██║███████╗");
+        getLogger().info(" ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝");
+        getLogger().info(" ");
+        getLogger().info(" ParkCore - Themepark Engine");
+        getLogger().info(" Version: " + getDescription().getVersion());
+        getLogger().info(" Author: NDGWebDesign");
+        getLogger().info(" ");
+    }
+
+    public static ParkCore getInstance() {
+        return instance;
+    }
+
 }
