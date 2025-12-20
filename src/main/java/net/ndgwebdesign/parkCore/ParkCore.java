233cfa0ec9cfa9158d03a21d1d24839484ed67a3
dafrijder <nielsdegast@outlook.com>
2025-12-20 11:04:13 +0100

    Initial commit

---
+++ src/main/java/net/ndgwebdesign/parkCore/ParkCore.java
@@ -0,0 +1,17 @@
+package net.ndgwebdesign.parkCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ParkCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
